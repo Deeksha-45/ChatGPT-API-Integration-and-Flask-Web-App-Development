3727e36bcf3e677dc8e6f01951308e1afa25e78e
Deeksha-45 <95526751+Deeksha-45@users.noreply.github.com>
2023-05-08 23:20:36 +0530

    app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import os
 import requests
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
-app.config['API_KEY'] = os.environ['OPENAI_API_KEY']
+app=Flask(__name__)
+app.config['API_KEY']=os.environ['OPENAI_API_KEY']
 
 @app.route('/')
 def index():
@@ -23,8 +23,8 @@ def results():
             'n': 1,
             'stop': '\n'
         }, headers={
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {app.config["API_KEY"]}'
+            'Content-Type':'application/json',
+            'Authorization':f'Bearer {app.config["API_KEY"]}'
         })
 
         response.raise_for_status()
@@ -32,7 +32,7 @@ def results():
         return render_template('results.html', message=message)
 
     except requests.exceptions.HTTPError as e:
-        return render_template('index.html', error='An error occurred while processing your request. Please try again later.')
+        return render_template('index.html', error='  An error occurred while processing your request. Please try again later ')
 
 if __name__ == '__main__':
     app.run(debug=True)
